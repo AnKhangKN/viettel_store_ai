44308612985fff3bedd26c5f04dd56af85cbf9dc
An Khang <minecraftcopyright1302@gmail.com>
2026-07-28 04:17:28 +0000

    update docker file

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -4,8 +4,14 @@ import os
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-env_path = BASE_DIR / ".env.development"
-load_dotenv(env_path)
+
+APP_ENV = os.getenv("APP_ENV", "development").lower()
+if APP_ENV == "production":
+    env_path = BASE_DIR / ".env.production"
+else:
+    env_path = BASE_DIR / ".env.development"
+
+load_dotenv(env_path, override=False)
 
 class Config:
 
